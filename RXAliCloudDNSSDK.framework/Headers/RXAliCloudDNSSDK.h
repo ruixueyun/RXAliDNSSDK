50fb5b8b35d4233dd65d220aa643470de007e5eb
chenhan <chenhan@jixiang.cn>
2024-08-26 18:11:42 +0800

    1.0.0

---
+++ RXAliCloudDNSSDK.framework/Headers/RXAliCloudDNSSDK.h
@@ -0,0 +1,18 @@
+//
+//  RXAliCloudDNSSDK.h
+//  RXAliCloudDNSSDK
+//
+//  Created by root11 on 2024/8/6.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RXAliCloudDNSSDK.
+FOUNDATION_EXPORT double RXAliCloudDNSSDKVersionNumber;
+
+//! Project version string for RXAliCloudDNSSDK.
+FOUNDATION_EXPORT const unsigned char RXAliCloudDNSSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RXAliCloudDNSSDK/PublicHeader.h>
+#import <RXAliCloudDNSSDK/RXAliCloudDNSSDKService.h>
+
